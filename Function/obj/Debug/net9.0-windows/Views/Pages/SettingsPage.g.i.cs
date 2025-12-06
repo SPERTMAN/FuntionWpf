cb1332447442671dc379baa2615e1de53b9e8048
护 刘 <l1184085840@outlook.com>
2025-12-06 21:21:01 +0800

    20251206

--- Function/obj/Debug/net9.0-windows/Views/Pages/SettingsPage.g.i.cs
+++ Function/obj/Debug/net9.0-windows/Views/Pages/SettingsPage.g.i.cs
@@ -82,7 +82,7 @@ namespace Function.Views.Pages {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Function;component/views/pages/settingspage.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Function;V1.0.0.0;component/views/pages/settingspage.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\Views\Pages\SettingsPage.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
